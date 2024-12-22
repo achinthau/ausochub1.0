42099ee6099e50288bf67c170e409b1ed9c00d18
macorera <ashwincorera@gmail.com>
2024-12-23 00:17:51 +0530

    export issue fixed

--- resources/views/livewire/reports/abandoned-call.blade.php
+++ resources/views/livewire/reports/abandoned-call.blade.php
@@ -10,7 +10,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            @if ($loadPosts)
+            @if ($readyToLoad)
                 @livewire('tables.abandoned-call-table')
             @else
                 Loading....

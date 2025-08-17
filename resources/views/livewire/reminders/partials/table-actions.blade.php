90fa636fcdcb649552808cbe4ddb3843e43ad4d4
Viraj Vikum <virajempire2001@gmail.com>
2025-08-17 23:31:39 +0530

    Developed call api for reminders

--- resources/views/livewire/reminders/partials/table-actions.blade.php
+++ resources/views/livewire/reminders/partials/table-actions.blade.php
@@ -18,7 +18,7 @@
 
 
             {{-- <button  class="bg-blue-500 text-white px-3 py-1 rounded whitespace-nowrap">complete</button> --}}
-            <svg class="w-5 h-5 cursor-pointer text-green-500 hover:text-green-600" xmlns="http://www.w3.org/2000/svg" fill="currentColor" stroke="none" viewBox="0 0 24 24"><path d="M20 10.999h2C22 5.869 18.127 2 12.99 2v2C17.052 4 20 6.943 20 10.999z"></path><path d="M13 8c2.103 0 3 .897 3 3h2c0-3.225-1.775-5-5-5v2zm3.422 5.443a1.001 1.001 0 0 0-1.391.043l-2.393 2.461c-.576-.11-1.734-.471-2.926-1.66-1.192-1.193-1.553-2.354-1.66-2.926l2.459-2.394a1 1 0 0 0 .043-1.391L6.859 3.513a1 1 0 0 0-1.391-.087l-2.17 1.861a1 1 0 0 0-.29.649c-.015.25-.301 6.172 4.291 10.766C11.305 20.707 16.323 21 17.705 21c.202 0 .326-.006.359-.008a.992.992 0 0 0 .648-.291l1.86-2.171a1 1 0 0 0-.086-1.391l-4.064-3.696z"></path></svg>
+            <svg class="w-5 h-5 cursor-pointer text-green-500 hover:text-green-600" wire:click="callApi({{ $callback->id }})" xmlns="http://www.w3.org/2000/svg" fill="currentColor" stroke="none" viewBox="0 0 24 24"><path d="M20 10.999h2C22 5.869 18.127 2 12.99 2v2C17.052 4 20 6.943 20 10.999z"></path><path d="M13 8c2.103 0 3 .897 3 3h2c0-3.225-1.775-5-5-5v2zm3.422 5.443a1.001 1.001 0 0 0-1.391.043l-2.393 2.461c-.576-.11-1.734-.471-2.926-1.66-1.192-1.193-1.553-2.354-1.66-2.926l2.459-2.394a1 1 0 0 0 .043-1.391L6.859 3.513a1 1 0 0 0-1.391-.087l-2.17 1.861a1 1 0 0 0-.29.649c-.015.25-.301 6.172 4.291 10.766C11.305 20.707 16.323 21 17.705 21c.202 0 .326-.006.359-.008a.992.992 0 0 0 .648-.291l1.86-2.171a1 1 0 0 0-.086-1.391l-4.064-3.696z"></path></svg>
 
             {{-- <button  class="bg-blue-500 text-white px-3 py-1 rounded whitespace-nowrap">close</button> --}}
             <a href="#"
@@ -65,3 +65,11 @@
 
     @endif
 </div>
+
+<script>
+    window.addEventListener('notify', event => {
+        console.log(event.detail.message); // ← this will show "API call successful!"
+        // You can replace this with toast, swal, alert, etc.
+        // alert(event.detail.message);
+    });
+</script>

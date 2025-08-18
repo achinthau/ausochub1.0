3971763e9f4eb0b2165c30fbb87f599a5b3c9912
Viraj Vikum <virajempire2001@gmail.com>
2025-08-18 17:44:31 +0530

    Updated call api in the reminder panel

--- app/Http/Livewire/Reminders/Partials/TableActions.php
+++ app/Http/Livewire/Reminders/Partials/TableActions.php
@@ -58,8 +58,8 @@ class TableActions extends Component
     //     }
     // }
     
-    $url = "123.231.74.22:8080/ausoadmin/dialscripts/dial.php";
-    // $url = env('CALL_SERVER_API_URL') .'/dialscripts/dial.php';
+    // $url = "123.231.74.22:8080/ausoadmin/dialscripts/dial.php";
+    $url = env('CALL_SERVER_API_URL') .'/dialscripts/dial.php';
 
     $response = $response = Http::get($url, [
         'type'   => 'out',

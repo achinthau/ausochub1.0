0d14ea44f9f67b1d84b5fa7314d88ebe7eba6ad8
Viraj Vikum <virajempire2001@gmail.com>
2025-09-06 20:53:35 +0530

    Updated to create a skill when create a campaign

--- app/Http/Livewire/Dialer/Settings/Campaign/Create.php
+++ app/Http/Livewire/Dialer/Settings/Campaign/Create.php
@@ -7,6 +7,7 @@ use App\Models\CampaignType;
 use App\Models\Company;
 use App\Models\Feed;
 use App\Models\User;
+use App\Repositories\ApiManager;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Livewire\Component;
@@ -114,7 +115,7 @@ class Create extends Component
         $this->campaignTypes = CampaignType::orderBy('name')->get(['id', 'name']);
         $this->feeds = Feed::orderBy('name')->get(['id', 'name']);
         $this->users = collect();
-        
+
 
         $campaign = Campaign::find($campaign_id);
         if ($campaign) {
@@ -194,23 +195,23 @@ class Create extends Component
     {
         $validated = $this->validate([
             'name' => 'required|string|max:255',
-        //     'company_id' => 'required|exists:companies,id',
-        //     'user_ids' => 'array|exists:users,id',
-        //     'feed_ids' => 'array|exists:feeds,id',
-        //     'schedule.monday.start' => 'nullable|date_format:h:i A',
-        //     'schedule.monday.end' => 'nullable|date_format:h:i A|after:schedule.monday.start',
-        //     'schedule.tuesday.start' => 'nullable|date_format:h:i A',
-        //     'schedule.tuesday.end' => 'nullable|date_format:h:i A|after:schedule.tuesday.start',
-        //     'schedule.wednesday.start' => 'nullable|date_format:h:i A',
-        //     'schedule.wednesday.end' => 'nullable|date_format:h:i A|after:schedule.wednesday.start',
-        //     'schedule.thursday.start' => 'nullable|date_format:h:i A',
-        //     'schedule.thursday.end' => 'nullable|date_format:h:i A|after:schedule.thursday.start',
-        //     'schedule.friday.start' => 'nullable|date_format:h:i A',
-        //     'schedule.friday.end' => 'nullable|date_format:h:i A|after:schedule.friday.start',
-        //     'schedule.saturday.start' => 'nullable|date_format:h:i A',
-        //     'schedule.saturday.end' => 'nullable|date_format:h:i A|after:schedule.saturday.start',
-        //     'schedule.sunday.start' => 'nullable|date_format:h:i A',
-        //     'schedule.sunday.end' => 'nullable|date_format:h:i A|after:schedule.sunday.start',
+            //     'company_id' => 'required|exists:companies,id',
+            //     'user_ids' => 'array|exists:users,id',
+            //     'feed_ids' => 'array|exists:feeds,id',
+            //     'schedule.monday.start' => 'nullable|date_format:h:i A',
+            //     'schedule.monday.end' => 'nullable|date_format:h:i A|after:schedule.monday.start',
+            //     'schedule.tuesday.start' => 'nullable|date_format:h:i A',
+            //     'schedule.tuesday.end' => 'nullable|date_format:h:i A|after:schedule.tuesday.start',
+            //     'schedule.wednesday.start' => 'nullable|date_format:h:i A',
+            //     'schedule.wednesday.end' => 'nullable|date_format:h:i A|after:schedule.wednesday.start',
+            //     'schedule.thursday.start' => 'nullable|date_format:h:i A',
+            //     'schedule.thursday.end' => 'nullable|date_format:h:i A|after:schedule.thursday.start',
+            //     'schedule.friday.start' => 'nullable|date_format:h:i A',
+            //     'schedule.friday.end' => 'nullable|date_format:h:i A|after:schedule.friday.start',
+            //     'schedule.saturday.start' => 'nullable|date_format:h:i A',
+            //     'schedule.saturday.end' => 'nullable|date_format:h:i A|after:schedule.saturday.start',
+            //     'schedule.sunday.start' => 'nullable|date_format:h:i A',
+            //     'schedule.sunday.end' => 'nullable|date_format:h:i A|after:schedule.sunday.start',
         ]);
 
         $formattedSchedule = $this->formatSchedule();
@@ -235,7 +236,19 @@ class Create extends Component
             $data['created_by'] = Auth::user()->id;
             Campaign::create($data);
             \Log::debug('Campaign Created', ['data' => $data]);
+
+
+
+            $data2 = [
+                ['name' => 'queueName', 'contents' => $this->name],
+                ['name' => 'mohClass', 'contents' => 'silence'],
+                ['name' => 'type', 'contents' => 'dialer'],
+            ];
+
+            $response = ApiManager::createSkill($data2);
         }
+
+
 
         $this->createCampaignModal = false;
         $this->emit('campaignTableUpdated');

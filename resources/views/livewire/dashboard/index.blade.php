84d062fc8abe476581fe3d33da170efeac28280e
Viraj Vikum <virajempire2001@gmail.com>
2025-09-06 18:19:51 +0530

    added bound selection option to dashboard

--- resources/views/livewire/dashboard/index.blade.php
+++ resources/views/livewire/dashboard/index.blade.php
@@ -10,61 +10,79 @@
 
                 {{-- <div class="absolute right-44 flex justify-between">
                     @livewire('dashboard.select-bound') not used
-                    
+
                 </div> --}}
-            {{-- </div> --}}
+                {{--
+            </div> --}}
             {{-- <div> --}}
-            <h2 class="flex-1 font-semibold text-xl text-gray-800 leading-tight ">
-                {{ __('Dashboard') }}
-            </h2>
+                <h2 class="flex-1 font-semibold text-xl text-gray-800 leading-tight ">
+                    {{ __('Dashboard') }}
+                </h2>
 
-            {{-- <div class="flex {{ !$isVisible ? 'pointer-events-none opacity-50' : '' }}"> --}}
-            <div class="flex">
-                <div>
-                    {{-- @dump(Auth::user()->has_queue) --}}
-                    {{-- @if (Auth::user()->has_queue) --}}
-                    <label><button type="button" onclick="Livewire.emitTo('dashboard.index', 'setOutbound')">Outbound</button></label>
+                {{-- <div class="flex {{ !$isVisible ? 'pointer-events-none opacity-50' : '' }}"> --}}
+                    <div class="flex">
+                        <div class="flex">
+                            {{-- @dump(Auth::user()->has_queue) --}}
+                            {{-- @if (Auth::user()->has_queue) --}}
 
-
-
-                    @if (Auth::user()->on_break && $user->agent_break_type != 'ACW')
-                        <div class="flex space-x-4">
-                            <div class="my-auto" {{-- x-data="appFooterComponent('{{ Auth::user()->break_started_at->format('Y/m/d H:i:s') }}')" --}}{{--  x-init="init()" --}}>
-                                <div>
-                                    <span class="bg-red-600 text-white rounded-md p-2"{{--  x-text="getTime()" --}}>On
-                                        Break</span>
-                                </div>
+                                <div class="flex items-center space-x-2 px-2">
+                                {{-- <label for="boundType" class="text-sm font-medium text-gray-700">
+                                    Bound Type:
+                                </label> --}}
+                                <select id="boundType"
+                                    class="block w-40 rounded-lg border border-gray-300 bg-white px-3 py-2 text-sm text-gray-700 shadow-sm focus:border-indigo-800 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                    onchange="Livewire.emitTo('dashboard.index', this.value === 'inbound' ? 'setInbound' : 'setOutbound')">
+                                    <option value="inbound" {{ $boundType === 'inbound' ? 'selected' : '' }}>Inbound
+                                    </option>
+                                    <option value="dialer" {{ $boundType === 'dialer' ? 'selected' : '' }}>Outbound
+                                    </option>
+                                </select>
                             </div>
-                            <x-button icon="clipboard-list" secondary label="End Break"
-                                onclick="Livewire.emitTo('dashboard.partials.agent-break', 'endBreak')" />
+
+
+
+
+                            @if (Auth::user()->on_break && $user->agent_break_type != 'ACW')
+                                <div class="flex space-x-4">
+                                    <div class="my-auto" {{--
+                                        x-data="appFooterComponent('{{ Auth::user()->break_started_at->format('Y/m/d H:i:s') }}')"
+                                        --}}{{-- x-init="init()" --}}>
+                                        <div>
+                                            <span class="bg-red-600 text-white rounded-md p-2" {{-- x-text="getTime()"
+                                                --}}>On
+                                                Break</span>
+                                        </div>
+                                    </div>
+                                    <x-button icon="clipboard-list" secondary label="End Break"
+                                        onclick="Livewire.emitTo('dashboard.partials.agent-break', 'endBreak')" />
+
+                                </div>
+                            @else
+                                <x-button id="start-break-button" icon="clipboard-list" secondary label="Start Break"
+                                    onclick="Livewire.emitTo('dashboard.partials.agent-break', 'showCreateUserBreakModal')"
+                                    :disabled="!$isVisible" />
+                            @endif
+                            {{-- @endif --}}
 
                         </div>
-                    @else
-                        <x-button id="start-break-button" icon="clipboard-list" secondary label="Start Break"
-                            onclick="Livewire.emitTo('dashboard.partials.agent-break', 'showCreateUserBreakModal')"
-                            :disabled="!$isVisible" />
-                    @endif
-                    {{-- @endif --}}
+                        <div class=" pl-8">
+                            {{-- @livewire('dashboard.select-bound') --}}
+                        </div>
 
+                        <script>
+                            window.addEventListener('updateButton', event => {
+                                const button = document.querySelector('#start-break-button');
+                                if (event.detail.isVisible) {
+                                    button.removeAttribute('disabled');
+                                } else {
+                                    button.setAttribute('disabled', true);
+                                }
+                            });
+                        </script>
+
+
+                    </div>
                 </div>
-                <div class=" pl-8">
-                    {{-- @livewire('dashboard.select-bound') --}}
-                </div>
-
-                <script>
-                    window.addEventListener('updateButton', event => {
-                        const button = document.querySelector('#start-break-button');
-                        if (event.detail.isVisible) {
-                            button.removeAttribute('disabled');
-                        } else {
-                            button.setAttribute('disabled', true);
-                        }
-                    });
-                </script>
-
-
-            </div>
-        </div>
     </x-slot>
 
     <div class="py-12" wire:poll.3000ms>
@@ -74,12 +92,12 @@
                 <div class="flex space-x-4">
                     <div class="w-3/4 space-y-4">
                         <div class="grid grid-cols-3 space-x-4">
-                            <x-dashboard.box title="Answered" :value="$user->agent->todayQueues->count()" iconBackground="bg-blue-100"
-                                name='phone' iconColor="text-blue-400" />
-                            {{--  <x-dashboard.box title="Missed" :value="$user->agent->todayQueues->count()" iconBackground="bg-red-100"
-                                name='phone-missed' iconColor="text-red-400" /> --}}
-                            <x-dashboard.box title="Missed" :value="$user->agent->miscall_count" iconBackground="bg-red-100"
-                                name='phone-missed' iconColor="text-red-400" />
+                            <x-dashboard.box title="Answered" :value="$user->agent->todayQueues->count()"
+                                iconBackground="bg-blue-100" name='phone' iconColor="text-blue-400" />
+                            {{-- <x-dashboard.box title="Missed" :value="$user->agent->todayQueues->count()"
+                                iconBackground="bg-red-100" name='phone-missed' iconColor="text-red-400" /> --}}
+                            <x-dashboard.box title="Missed" :value="$user->agent->miscall_count"
+                                iconBackground="bg-red-100" name='phone-missed' iconColor="text-red-400" />
                             <x-dashboard.box title="Total Break" :value="$totalBreakTime" iconBackground="bg-yellow-100"
                                 name='break' iconColor="text-yellow-400" />
 
@@ -87,8 +105,8 @@
 
                         <div class="grid grid-cols-3 space-x-4">
                             <a href="{{ route('chat.index') }}">
-                            <x-dashboard.box title="Messages" :value="$messagesCount" iconBackground="bg-red-100" name='message'
-                                iconColor="text-red-400" /> </a>
+                                <x-dashboard.box title="Messages" :value="$messagesCount" iconBackground="bg-red-100"
+                                    name='message' iconColor="text-red-400" /> </a>
                             <div class="bg-white p-6 rounded-md shadow-md space-y-2">
                                 <h1 class="text-xs text-gray-400 font-semibold">Skills</h1>
                                 <hr>
@@ -99,8 +117,7 @@
                                             {{ $skill }}
                                         </label>
                                         @if (config('auso.allow_skill_change'))
-                                            <x-toggle lg wire:model="selectedSkills.{{ $skill }}"
-                                                value="{{ $skill }}" />
+                                            <x-toggle lg wire:model="selectedSkills.{{ $skill }}" value="{{ $skill }}" />
                                         @endif
                                     </div>
                                 @endforeach
@@ -112,25 +129,26 @@
 
                                 @foreach ($queueWiseData as $data)
                                     <div class="flex ">
-                                        <div
-                                            class="flex-1  text-sm font-medium text-secondary-700 dark:text-gray-400 mr-2">
-                                            {{ $data->queuename }}</div>
+                                        <div class="flex-1  text-sm font-medium text-secondary-700 dark:text-gray-400 mr-2">
+                                            {{ $data->queuename }}
+                                        </div>
                                         <div>{{ $data->queue_wating_count }}</div>
                                     </div>
                                 @endforeach
                             </div>
 
                             {{-- <div class="bg-white p-6 rounded-md shadow-md space-y-2">
-                        <h1 class="text-xs text-gray-400 font-semibold">Break</h1>
-                        <hr>
-                        @if (Auth::user()->on_break)
-                            <x-countdown class="text-green-600 font-semibold" :expires="Auth::user()->break_started_at">
-                                <span x-text="timer.hours">{{ $component->hours() }}</span> hours
-                                <span x-text="timer.minutes">{{ $component->minutes() }}</span> minutes
-                                <span x-text="timer.seconds">{{ $component->seconds() }}</span> seconds
-                            </x-countdown>
-                        @endif
-                    </div> --}}
+                                <h1 class="text-xs text-gray-400 font-semibold">Break</h1>
+                                <hr>
+                                @if (Auth::user()->on_break)
+                                <x-countdown class="text-green-600 font-semibold"
+                                    :expires="Auth::user()->break_started_at">
+                                    <span x-text="timer.hours">{{ $component->hours() }}</span> hours
+                                    <span x-text="timer.minutes">{{ $component->minutes() }}</span> minutes
+                                    <span x-text="timer.seconds">{{ $component->seconds() }}</span> seconds
+                                </x-countdown>
+                                @endif
+                            </div> --}}
                         </div>
 
                         <div class="grid grid-cols-3  gap-4">
@@ -157,41 +175,41 @@
         // var FULL_SOCKET_URL = SOCKET_URL + ":" + SOCKET_PORT;
 
 
-        var SOCKET_URL  = "<?php echo env('SOCKET_SERVER_URL', 'http://localhost'); ?>";
+        var SOCKET_URL = "<?php echo env('SOCKET_SERVER_URL', 'http://localhost'); ?>";
         var SOCKET_PORT = "<?php echo env('SOCKET_SERVER_PORT', '3000');?>";
 
-        var FULL_SOCKET_URL = SOCKET_URL + ":" + SOCKET_PORT;
-        
+        var FULL_SOCKET_URL = SOCKET_URL + ":" + SOCKET_PORT;
+
         const socket = io(FULL_SOCKET_URL, {
-        transports: ['websocket'],
-        secure: true
-    });
+            transports: ['websocket'],
+            secure: true
+        });
 
-    socket.on('connect', () => {
-        console.log('Connected to Socket.IO server');
-    });
+        socket.on('connect', () => {
+            console.log('Connected to Socket.IO server');
+        });
 
-      
+
         var openedTab = null; // Store the reference to the opened tab
-    
+
         socket.on('call.answered', (data) => {
-    
+
             var url = "/leads/"; // Set the target URL
-    
+
             console.log('Lead Answered-----------:', data);
-            
-    
-    
-        if (openedTab && !openedTab.closed) {
+
+
+
+            if (openedTab && !openedTab.closed) {
                 // If the tab is already open, update its location
                 openedTab.location.href = url + data;
                 openedTab.focus
-    } else {
+            } else {
                 // If no tab is open, create a new one
                 openedTab = window.open(url + data, "_blank");
             }
         });
-    
+
     </script>
 
 

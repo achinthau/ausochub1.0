55b506b99daabbef257247e681a16e3e44ff4eb3
Viraj Vikum <virajempire2001@gmail.com>
2025-09-18 21:10:07 +0530

    Updated campaign create to save name in camelCase

--- app/Http/Livewire/CampaignTable.php
+++ app/Http/Livewire/CampaignTable.php
@@ -23,9 +23,9 @@ class CampaignTable extends DataTableComponent
     }
 
     public function builder(): Builder
-{
-    return Campaign::query()->with('creator')->with('companies')->with('types');
-}
+    {
+        return Campaign::query()->with('creator')->with('companies')->with('types');
+    }
 
     public function columns(): array
     {
@@ -33,44 +33,48 @@ class CampaignTable extends DataTableComponent
             Column::make("Id", "id")
                 ->sortable(),
             Column::make("Name", "name")
-                ->sortable(),
-                Column::make("Type")
-            ->format(fn($value, $row) => $row->types?->name ?? 'N/A'),
-            Column::make("Status", "status")
-            ->sortable()
-            ->format(function ($value, $row) {
-                $statusMap = [
-                    0 => 'Inactive (Not Started)',
-                    1 => 'Active (Running)',
-                    2 => 'On Hold',
-                    3 => 'Completed',
-                    4 => 'Canceled',
-                ];
-                return $statusMap[$value] ?? 'Unknown';
-            }),
-            
-        Column::make("Assigned Users", "assigned_users")
-            ->format(fn($value, $row) => 
-                implode(', ', \App\Models\User::whereIn('id', explode(',', $row->assigned_users))->pluck('name')->toArray())
-            ),
+                ->sortable()
+                ->format(fn($value) => ucwords(preg_replace('/([a-z])([A-Z])/', '$1 $2', $value))),
 
-        
-        Column::make("Assigned Feeds", "assigned_feeds")
-            ->format(fn($value, $row) => 
-                implode(', ', \App\Models\Feed::whereIn('id', explode(',', $row->assigned_feeds))->pluck('name')->toArray())
-            ),
+            Column::make("Type")
+                ->format(fn($value, $row) => $row->types?->name ?? 'N/A'),
+            Column::make("Status", "status")
+                ->sortable()
+                ->format(function ($value, $row) {
+                    $statusMap = [
+                        0 => 'Inactive (Not Started)',
+                        1 => 'Active (Running)',
+                        2 => 'On Hold',
+                        3 => 'Completed',
+                        4 => 'Canceled',
+                    ];
+                    return $statusMap[$value] ?? 'Unknown';
+                }),
+
+            Column::make("Assigned Users", "assigned_users")
+                ->format(
+                    fn($value, $row) =>
+                    implode(', ', \App\Models\User::whereIn('id', explode(',', $row->assigned_users))->pluck('name')->toArray())
+                ),
+
+
+            Column::make("Assigned Feeds", "assigned_feeds")
+                ->format(
+                    fn($value, $row) =>
+                    implode(', ', \App\Models\Feed::whereIn('id', explode(',', $row->assigned_feeds))->pluck('name')->toArray())
+                ),
             Column::make("Company")
-            ->format(fn($value, $row) => $row->companies?->name ?? 'N/A'),
-            
-                Column::make("Created by")
-            ->format(fn($value, $row) => $row->creator?->name ?? 'N/A'),
+                ->format(fn($value, $row) => $row->companies?->name ?? 'N/A'),
+
+            Column::make("Created by")
+                ->format(fn($value, $row) => $row->creator?->name ?? 'N/A'),
             // Column::make("Created at", "created_at")
             //     ->sortable(),
             // Column::make("Updated at", "updated_at")
             //     ->sortable(),
             // Column::make("Scheduled time", "schedule")
             //     ->sortable(),
-                Column::make("Actions")
+            Column::make("Actions")
                 ->label(fn($row) => view('livewire.dialer.settings.campaign.table-action', ['feed' => $row]))
                 ->html(),
         ];

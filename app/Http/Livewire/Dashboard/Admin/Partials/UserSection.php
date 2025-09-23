bec0f7e07e31435880c76851451fc6d7b5b7c084
Viraj Vikum <virajempire2001@gmail.com>
2025-09-23 10:26:30 +0530

    Updated agent logout events

--- app/Http/Livewire/Dashboard/Admin/Partials/UserSection.php
+++ app/Http/Livewire/Dashboard/Admin/Partials/UserSection.php
@@ -21,55 +21,59 @@ class UserSection extends Component
         $raisedHands = [];
 
         $loggedUserIds = DB::table('sessions')
-        ->whereNotNull('user_id')
-        ->pluck('user_id')
-        ->unique()
-        ->toArray();
-        
+            ->whereNotNull('user_id')
+            ->pluck('user_id')
+            ->unique()
+            ->toArray();
+
         $inboundUsers = [];
-    $dialerUsers = [];
+        $dialerUsers = [];
 
-         // Select Redis DB 1
 
-    foreach ($users as $agent) {
-        $userId = optional($agent->user)->id;
-        if ($userId) {
-            // Redis::select(0);
-            $key = "hand_raised:{$userId}";
-            if (Redis::get($key)) {
-                $raisedHands[$userId] = true;
-            }
-        }
-
-        if ($agent->user) {
-            // Redis::select(0);
-            $key = "user:{$userId}:bound_type";
-            $callDirection = Redis::get($key);
-            
-
-            if ($callDirection == 'inbound') {
-                $inboundUsers[] = $agent->user->id;
-                // dd('inbound');
-            } elseif ($callDirection == 'dialer') {
-                $dialerUsers[] = $agent->user->id;
-                // dd('dialer');
+        foreach ($users as $agent) {
+            $userId = optional($agent->user)->id;
+            if ($userId) {
+                // Redis::select(0);
+                $key = "hand_raised:{$userId}";
+                if (Redis::get($key)) {
+                    $raisedHands[$userId] = true;
+                }
             }
 
-            
+            if ($agent->user) {
+                // Redis::select(0);
+                $key = "user:{$userId}:bound_type";
+                $callDirection = Redis::get($key);
+
+
+                if ($callDirection == 'inbound') {
+                    $inboundUsers[] = $agent->user->id;
+                    // dd('inbound');
+                } elseif ($callDirection == 'dialer') {
+                    $dialerUsers[] = $agent->user->id;
+                    // dd('dialer');
+                }
+
+
+            }
+
+
         }
-
-
-    }
-    // dd($inboundUsers);
+        // dd($inboundUsers);
 
         return view(
             'livewire.dashboard.admin.partials.user-section',
-            ['users' => $users,'loggedUserIds' => $loggedUserIds,'inboundUsers' => $inboundUsers,
-        'dialerUsers' => $dialerUsers, 'raisedHands' => $raisedHands,]
+            [
+                'users' => $users,
+                'loggedUserIds' => $loggedUserIds,
+                'inboundUsers' => $inboundUsers,
+                'dialerUsers' => $dialerUsers,
+                'raisedHands' => $raisedHands,
+            ]
         );
     }
 
-    
+
     public function listenCall($extension, $extenType, $action)
     {
         $data = [
@@ -89,7 +93,7 @@ class UserSection extends Component
                 'name' => 'action',
                 'contents' => $action
             ],
-            
+
         ];
         Log::info('Calling ApiManager::listenCall with data: ', $data);
         ApiManager::listentCall($data);

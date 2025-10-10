f867fe4d693c8522e1783c0dd9f90503f4d4885b
Viraj Vikum <virajempire2001@gmail.com>
2025-10-10 12:51:49 +0530

    Fixed tickets table to export data with headings

--- app/Exports/TicketExport.php
+++ app/Exports/TicketExport.php
@@ -4,10 +4,11 @@ namespace App\Exports;
 
 use App\Models\Ticket;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
 use PhpOffice\PhpSpreadsheet\Shared\Date;
 
-class TicketExport implements FromCollection
+class TicketExport implements FromCollection, WithMapping, WithHeadings
 {
     /**
      * @return \Illuminate\Support\Collection
@@ -22,5 +23,44 @@ class TicketExport implements FromCollection
     public function __construct($tickets)
     {
         $this->tickets = $tickets;
+    }
+
+    public function map($ticket): array
+    {
+        return [
+            $ticket->id,
+            $ticket->topic,
+            $ticket->description,
+            $ticket->lead?->contact_number,
+            $ticket->category?->title,
+            $ticket->subCategory?->title,
+            $ticket->tags ? implode(', ', $ticket->tags) : null,
+            $ticket->outlet?->title,
+            $ticket->department?->name,
+            $ticket->assignedUser?->name,
+            $ticket->due_at,
+            $ticket->created_at,
+            $ticket->updated_at,
+        ];
+    }
+
+    // Column headers
+    public function headings(): array
+    {
+        return [
+            'ID',
+            'Topic',
+            'Description',
+            'Contact',
+            'Category',
+            'Sub Category',
+            'Tags',
+            'Outlet',
+            'Department',
+            'Assigned To',
+            'Due At',
+            'Created At',
+            'Updated At',
+        ];
     }
 }

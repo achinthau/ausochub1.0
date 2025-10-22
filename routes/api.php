af423468b6a2db79ddf5ac448a5b54e768bf0dc0
Viraj Vikum <virajempire2001@gmail.com>
2025-10-22 23:14:26 +0530

    Updated the ongoing call count

--- routes/api.php
+++ routes/api.php
@@ -93,7 +93,7 @@ Route::post('/call-answered', function (StoreAnsweredCall $request) {
 
     Cache::increment('current-call-count');
     Cache::increment($request['queuename'] . "-current-call-count");
-    Cache::forever('agent-on-call-' . $agent->id . '-' . $request['queuename'] , 1);
+    Cache::add('agent-on-call-' . $agent->id . '-' . $request['queuename'] , 1);
 
 
 

a04f2cd583a0563e4ef4523d12b276e3c57834e6
Viraj Vikum <virajempire2001@gmail.com>
2025-11-06 13:28:23 +0530

    Updated the dialer panel

--- app/Models/CxTicket.php
+++ app/Models/CxTicket.php
@@ -9,13 +9,14 @@ class CxTicket extends Model
 {
     use HasFactory;
 
+    protected $table = 'cx_tickets';
     protected $fillable = [
         'category', 'product', 'model', 'work_order_no', 'service_center', 'warranty_status', 'sold_date',
         'customer_name', 'customer_address', 'customer_contact_01', 'customer_contact_02',
         'technician_name', 'technician_contact', 'supervisor_name', 'supervisor_contact', 'status', 'creator',
         'satisfaction_rate','satisfaction_reasons','dis_satisfaction_reasons','cancelling_reasons',
         'closed_by', 'surveyed_by', 'company', 'reopened_by', 'reopened_reasons', 'skipped_reasons','skipped_by',
-        'cancelling_comment','change_request','updated_at',
+        'cancelling_comment','change_request',
     ];
     
 }

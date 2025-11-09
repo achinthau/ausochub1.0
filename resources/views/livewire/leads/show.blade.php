a59f64d8280bb54493f8ad505e1cf6160033e5be
Viraj Vikum <virajempire2001@gmail.com>
2025-11-09 16:23:20 +0530

    Updated follow up dialer to show reaction buttons within a minute

--- resources/views/livewire/leads/show.blade.php
+++ resources/views/livewire/leads/show.blade.php
@@ -678,8 +678,8 @@
                                                             $isNew = is_null($contact->status);
 
                                                             // Check if Not Answered was submitted today
-                                                            $lastUpdated = \Carbon\Carbon::parse($contact->next_available_at);
-                                                            $hideNotAnsweredButtons = $isNotAnswered && $lastUpdated->greaterThan(now()->startOfDay());
+                                                            $lastUpdated = \Carbon\Carbon::parse($contact->updated_at);
+                                                            $hideNotAnsweredButtons = $isNotAnswered && $lastUpdated->greaterThan(\Carbon\Carbon::now()->subMinutes(1));
                                                             $notAnsweredCount = strlen((string) $contact->status);
                                                         @endphp
 

01a397169b4c624697568a3eb853cfc696e17f92
Viraj Vikum <virajempire2001@gmail.com>
2026-02-01 14:26:27 +0530

    Hourly call count report and graph for daily call summary report

--- resources/views/livewire/reports/partials/daily-queue-summary-actions.blade.php
+++ resources/views/livewire/reports/partials/daily-queue-summary-actions.blade.php
@@ -1,5 +1,19 @@
 <div class="flex space-x-1">
-    <a href="{{ route('reports.daily-queue-summary-hourly', ['date' => $row->date, 'queue' => $row->queue]) }}" class="inline-flex items-center px-3 py-1 bg-blue-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:border-blue-700 focus:ring focus:ring-blue-200 disabled:opacity-25 transition">
+    <!-- <a href="{{ route('reports.daily-queue-summary-hourly', ['date' => $row->date, 'queue' => $row->queue]) }}" class="inline-flex items-center px-3 py-1 bg-blue-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:border-blue-700 focus:ring focus:ring-blue-200 disabled:opacity-25 transition">
         View
+    </a> -->
+
+    <div class="flex space-x-1 justify-around">
+    <a href="{{ route('reports.daily-queue-summary-hourly', ['date' => $row->date, 'queue' => $row->queue]) }}"
+        class="p-1 text-teal-600 hover:bg-teal-600 hover:text-white rounded">
+        <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <path d="M10 12a2 2 0 100-4 2 2 0 000 4z"></path>
+            <path fill-rule="evenodd"
+                d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
+                clip-rule="evenodd"></path>
+        </svg>
     </a>
+
+    
+</div>
 </div>

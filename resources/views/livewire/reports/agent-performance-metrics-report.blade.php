f9c763b3733cad87402ed280a5dbe1da4beb7b1c
Viraj Vikum <virajempire2001@gmail.com>
2026-04-04 15:09:52 +0530

    Feat: agent performance report

--- resources/views/livewire/reports/agent-performance-metrics-report.blade.php
+++ resources/views/livewire/reports/agent-performance-metrics-report.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         <div class="flex">
             <h2 class="flex-1 font-semibold text-xl text-gray-800 leading-tight">
-                {{ __('Call Agent Performance Report') }}
+                {{ __('Agent Performance Report') }}
             </h2>
         </div>
     </x-slot>

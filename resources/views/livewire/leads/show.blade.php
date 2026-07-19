b251e750246c07eb2b7d2bb0791ea9c747c0fbfd
Viraj Vikum <virajempire2001@gmail.com>
2026-07-19 13:05:18 +0530

    ticket overview lable

--- resources/views/livewire/leads/show.blade.php
+++ resources/views/livewire/leads/show.blade.php
@@ -294,8 +294,8 @@
                     </div> --}}
                     <details class="bg-white p-4 space-y-2 text-xs rounded-md shadow-sm" @if($ticketInfoOpen) open @endif x-on:toggle="$wire.set('ticketInfoOpen', $event.target.open)">
                         <summary class="font-bold cursor-pointer list-none flex items-center justify-between">
-                            <span>Ticket Information</span>
-                            <span class="text-gray-400 text-xs">{{ $ticketInfoOpen ? 'Click to collapse' : 'Click to expand' }}</span>
+                            <span>Ticket Overview & Callback</span>
+                            <span class="text-gray-400 text-xs">{{ $ticketInfoOpen ? 'Click to collapse >>' : 'Click to expand  >>' }}</span>
                         </summary>
                         <hr>
 

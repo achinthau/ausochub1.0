5c017457ed36bf8ce6bb52ff6f30afae63bc850e
Viraj Vikum <virajempire2001@gmail.com>
2026-08-08 14:21:34 +0530

    Update dialer change request

--- app/Http/Livewire/Leads/Show.php
+++ app/Http/Livewire/Leads/Show.php
@@ -488,6 +488,11 @@ class Show extends Component
                 })
                 ->get();
 
+            if ($this->feedContacts->isNotEmpty()) {
+                FeedContactValid::whereIn('id', $this->feedContacts->pluck('id')->unique()->values())
+                    ->update(['assigned_to' => Auth::id()]);
+            }
+
 
             if ($this->feedContacts->isNotEmpty()) {
                 $foundContact = $this->feedContacts->first();
@@ -766,6 +771,11 @@ class Show extends Component
 
         if ($contacts->isEmpty()) {
             return collect();
+        }
+
+        if ($this->boundType === 'dialer') {
+            FeedContactValid::whereIn('id', $contacts->pluck('id')->unique()->values())
+                ->update(['assigned_to' => Auth::id()]);
         }
 
         $cxTicketColumns = ['category', 'product', 'model', 'work_order_no', 'service_center', 'warranty_status', 'sold_date', 'customer_name', 'customer_address', 'customer_contact_01', 'customer_contact_02', 'technician_name', 'technician_contact', 'supervisor_name', 'supervisor_contact', 'status', 'creator', 'satisfaction_rate', 'satisfaction_reasons', 'dis_satisfaction_reasons', 'cancelling_reasons', 'closed_by', 'surveyed_by', 'company', 'reopened_by', 'reopened_reasons', 'skipped_reasons', 'skipped_by', 'cancelling_comment', 'change_request'];

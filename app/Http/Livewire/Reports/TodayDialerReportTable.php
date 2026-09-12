c022b825ad6eb5c0d20f1a19dbe2a7f4db45fb33
Viraj Vikum <virajempire2001@gmail.com>
2026-09-12 19:54:24 +0530

    feat: date filter for admins in dialer reports

--- app/Http/Livewire/Reports/TodayDialerReportTable.php
+++ app/Http/Livewire/Reports/TodayDialerReportTable.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire\Reports;
 
+use App\Http\Livewire\Reports\Filters\DateRangeFilter;
 use App\Models\Campaign;
 use App\Models\Company;
 use App\Models\FeedContactValid;
@@ -13,7 +14,6 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 use Rappasoft\LaravelLivewireTables\DataTableComponent;
 use Rappasoft\LaravelLivewireTables\Views\Column;
-use Rappasoft\LaravelLivewireTables\Views\Filters\DateFilter;
 use Rappasoft\LaravelLivewireTables\Views\Filters\SelectFilter;
 use Rappasoft\LaravelLivewireTables\Views\Filters\TextFilter;
 
@@ -286,10 +286,17 @@ class TodayDialerReportTable extends DataTableComponent
 
         if ($this->isSuperAdmin()) {
             array_splice($filters, 1, 0, [
-                DateFilter::make('Called At')
-                    ->filter(function (Builder $builder, string $value) {
-                        if ($value !== '') {
-                            $builder->whereDate('attempted_at', $value);
+                DateRangeFilter::make('Called At')
+                    ->filter(function (Builder $builder, array $value) {
+                        $from = $value['from'] ?? '';
+                        $to = $value['to'] ?? '';
+
+                        if ($from !== '') {
+                            $builder->whereDate('attempted_at', '>=', $from);
+                        }
+
+                        if ($to !== '') {
+                            $builder->whereDate('attempted_at', '<=', $to);
                         }
                     }),
             ]);

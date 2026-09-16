3f1b3631f133da60ae4832e0602a29632822a8bf
Viraj Vikum <virajempire2001@gmail.com>
2026-09-16 18:49:39 +0530

    Feat: dashboard dialer counts

--- app/Http/Livewire/Dashboard/Index.php
+++ app/Http/Livewire/Dashboard/Index.php
@@ -238,6 +238,18 @@ class Index extends Component
             ->whereDate('attempted_at', today())
             ->count();
 
+        $notCompleted = FeedContactValid::whereIn('feed_id', $feedIds)
+            ->where('updated_by', $userId)
+            ->whereIn('status', [3,5,51,52,6])
+            ->whereDate('attempted_at', today())
+            ->count();
+
+        $finishedNotcompleted = FeedContactValidReport::whereIn('feed_id', $feedIds)
+            ->where('updated_by', $userId)
+            ->whereIn('status', [42])
+            ->whereDate('attempted_at', today())
+            ->count();
+
         $this->answeredCallsToday = FeedContactValidReport::whereIn('feed_id', $feedIds)
             ->where('updated_by', $userId)
             ->whereIn('status', [1, 41])
@@ -251,6 +263,7 @@ class Index extends Component
             ->count();
 
         $this->dialedCallsToday = $this->dialedCallsToday + $this->answeredCallsToday + $this->notAnsweredCallsToday ;
+        $this->notAnsweredCallsToday = $this->notAnsweredCallsToday + $notCompleted + $finishedNotcompleted ;
     }
 
     public function updatedSelectedSkills($type, $value)
